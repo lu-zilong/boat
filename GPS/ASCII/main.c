9388b9a0eafc3140fab326b1e3c2a92c9801582d
lu-zilong <951462045@qq.com>
2021-09-17 14:23:25 +0800

    修改传给闫的数据

--- GPS/ASCII/main.c
+++ GPS/ASCII/main.c
@@ -276,9 +276,7 @@ int main()
             // {
                 // sprintf(sendtoclient, "%.3f,%.7lf,%.7lf,%.2f,%.2f,%.2f", gps_data.gps_sec, temp_send[j],  temp_send[j + 1], gps_data.ve,  gps_data.vn,  gps_data.heading); 
                 // sprintf(sendtoclient, "%.3f,%.7lf,%.7lf,%.2f,%.2f,%.2f,", gps_data.gps_sec, gps_data.latitude,  gps_data.longitude,  gps_data.ve,  gps_data.vn,  gps_data.heading); 
-                sprintf(sendtoclient, "%.3f,%.7lf,%.7lf,%.2f,%.2f,%.2f,%.2f,", gps_data.gps_sec, gps_data.latitude,  gps_data.longitude,  gps_data.ve,  gps_data.vn,  gps_data.heading, w_z); 
-                // printf("sendtoclient = %s\n", sendtoclient);
-                n = sendto(server_sockfd, sendtoclient, sizeof(sendtoclient), 0, (struct sockaddr *)& client_sockaddr, len_client); // 扔给上
+            
             //     j = j + 2;
             // }
             // i++;
@@ -292,6 +290,9 @@ int main()
             hour = (clock_time & 0xFF0000) >> 16;
             minute = (clock_time & 0xFF00) >> 8;
             second = clock_time & 0xFF;
+            sprintf(sendtoclient, "%.3f,%.7lf,%.7lf,%.2f,%.2f,%.2f,%.2f,%d,%d,%d,%d", gps_data.gps_sec, gps_data.latitude,  gps_data.longitude,  gps_data.ve,  gps_data.vn,  gps_data.heading, w_z，hour,minute,second,jy); 
+                // printf("sendtoclient = %s\n", sendtoclient);
+            n = sendto(server_sockfd, sendtoclient, sizeof(sendtoclient), 0, (struct sockaddr *)& client_sockaddr, len_client); // 扔给上
             sprintf(boat_status, "%d,%d,%d,%d,%lf,%d,%d,%d,%d,%d,%d,%d,%.2f", hour, minute, second, receivefromClient[1], rud, receivefromClient[3], 
                             receivefromClient[4], side_rev, jx, jy, jz, receivefromClient[13], w_z); 
 
